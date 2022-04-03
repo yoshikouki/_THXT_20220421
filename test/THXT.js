e840b3f4374766d063001243a0f048b5c08237e2
yoshikouki <yoshikouki@gmail.com>
2022-04-03 11:26:18 +0900

    test for Mint

--- test/THXT.js
+++ test/THXT.js
@@ -26,4 +26,16 @@ describe("THXT contract", () => {
       expect(await THXT.totalSupply()).to.equal(ownerBalance);
     });
   });
+
+  describe("Mint", () => {
+    it("Should mint tokens", async () => {
+      expect(await THXT.totalSupply()).to.equal(0);
+      expect(await THXT.balanceOf(owner.address)).to.equal(0);
+      const mint0Tx = await THXT.connect(owner).mint(owner.address);
+      await mint0Tx.wait();
+      expect(await THXT.totalSupply()).to.equal(1);
+      expect(await THXT.balanceOf(owner.address)).to.equal(1);
+      expect(await THXT.ownerOf(0)).to.equal(owner.address);
+    });
+  });
 });
